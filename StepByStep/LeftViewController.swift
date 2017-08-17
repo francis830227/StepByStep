a9b622199c0844aa09322a1e89987b95223b13f9
francis830227 <francis830227@gmail.com>
2017-08-17 19:56:09 +0800

    toSlideMenu

--- StepByStep/LeftViewController.swift
+++ StepByStep/LeftViewController.swift
@@ -7,29 +7,18 @@
 //
 
 import UIKit
+import SwifterSwift
 
 class LeftViewController: UIViewController {
 
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    
+    @IBOutlet weak var userNameLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        backgroundImageView.blur(withStyle: .regular)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
